266a3c17dea4cbb547ceced25db0268838af52d7
Pierre de Vésian <pierre.devesian@probesys.com>
2017-02-08 15:12:33 +0100

    Fix: select was appearing on other pages
        * Fix indentation
        * Add HTTPS URL

--- setup.php
+++ setup.php
@@ -12,7 +12,7 @@ function plugin_init_father() {
    global $PLUGIN_HOOKS;
    $PLUGIN_HOOKS['csrf_compliant']['father'] = true;
 
-  if (class_exists('PluginFatherFather')) { 
+  if (class_exists('PluginFatherFather')) {
    $config = new PluginFatherConfig();
    $PLUGIN_HOOKS['csrf_compliant']['father'] = true;
    $PLUGIN_HOOKS['config_page']['father'] = 'front/config.form.php';
@@ -20,33 +20,32 @@ function plugin_init_father() {
    Plugin::registerClass('PluginFatherFatherItem',
             array('addtabon' => array('PluginFatherFather')));
 
-   if (
-	   ((strpos($_SERVER['REQUEST_URI'], "ticket.form.php") !== false)&& $config->isOk(0)) || 
-	   ((strpos($_SERVER['REQUEST_URI'], "problem.form.php") !== false)&& $config->isOk(1)) || 
-	   ((strpos($_SERVER['REQUEST_URI'], "change.form.php") !== false)&& $config->isOK(2)) 
+    if (
+	   ((strpos($_SERVER['REQUEST_URI'], "/ticket.form.php") !== false)&& $config->isOk(0)) ||
+	   ((strpos($_SERVER['REQUEST_URI'], "/problem.form.php") !== false)&& $config->isOk(1)) ||
+	   ((strpos($_SERVER['REQUEST_URI'], "/change.form.php") !== false)&& $config->isOK(2))
     ) {
+        $PLUGIN_HOOKS['add_javascript']['father'][] = 'js/show_father.js';
+    }
 
-	   $PLUGIN_HOOKS['add_javascript']['father'][] = 'js/show_father.js';
-      }
-      if ($config->isOk(0)){
-	      $PLUGIN_HOOKS['pre_item_update']['father']['Ticket'] = array('PluginFatherFather', 'beforeUpdate');;
-      }
-      if ($config->isOk(1)){
-	      $PLUGIN_HOOKS['pre_item_update']['father']['Problem'] = array('PluginFatherFather', 'beforeUpdate');;
-      }
-  
-      if ($config->isOk(2)){
-	      $PLUGIN_HOOKS['pre_item_update']['father']['Change'] = array('PluginFatherFather', 'beforeUpdate');;
-      }
+    if ($config->isOk(0)){
+      $PLUGIN_HOOKS['pre_item_update']['father']['Ticket'] = array('PluginFatherFather', 'beforeUpdate');;
+    }
+    if ($config->isOk(1)){
+      $PLUGIN_HOOKS['pre_item_update']['father']['Problem'] = array('PluginFatherFather', 'beforeUpdate');;
+    }
 
+    if ($config->isOk(2)){
+      $PLUGIN_HOOKS['pre_item_update']['father']['Change'] = array('PluginFatherFather', 'beforeUpdate');;
+    }
   }
-  }
+}
 
 function plugin_version_father() {
    return array('name'       => __('Father&Sons', 'father'),
             'version'        => '1.0.1',
             'author'         => 'zorm (<a href="http://www.probesys.com">Probesys</a>)',
-            'homepage'       => 'http://www.probesys.com',
+            'homepage'       => 'https://www.probesys.com',
             'license'        => '<a href="../plugins/father/LICENSE" target="_blank">AGPLv3</a>',
             'minGlpiVersion' => "0.90");
 }
