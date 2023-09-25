ca3a4927841c8b403f14b56f84f8174cf5b4f511
Philippe GODOT <philippe.godot@probesys.com>
2023-09-25 15:40:54 +0200

    inject licence header on php files

--- inc/father.class.php
+++ inc/father.class.php
@@ -1,4 +1,31 @@
 <?php
+/**
+ * ---------------------------------------------------------------------
+ *  father is a plugin to add relation father / son between tickets
+ *  ---------------------------------------------------------------------
+ *  LICENSE
+ *
+ *  This file is part of father.
+ *
+ *  father is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  father is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with father. If not, see <http://www.gnu.org/licenses/>.
+ *  ---------------------------------------------------------------------
+ *  @copyright Copyright © 2022-2023 probeSys'
+ *  @license   http://www.gnu.org/licenses/agpl.txt AGPLv3+
+ *  @link      https://github.com/Probesys/glpi-plugins-father
+ *  @link      https://plugins.glpi-project.org/#/plugin/father
+ *  ---------------------------------------------------------------------
+ */
 
 class PluginFatherFather extends CommonDBTM
 {
